1dcfdacb51cfea22b0007517d028e7657e1fb846
0131ryu <131ryuji@gmail.com>
2022-06-05 20:22:14 +0900

    영화 사이트 만들기 복습- footer

---
+++ BOXOFFICE/src/script.js
@@ -0,0 +1,72 @@
+let moiveData = [
+  {
+    title: "위니브 월드 : 새로운 시대",
+    type: "판타지",
+    rate: "9.05",
+    openDate: "2021.03.27",
+    act: "자바독, 소울독, 개리씨, 파이, 캣",
+  },
+  {
+    title: "눈떠보니 코딩테스트 전날 with. JAVA",
+    type: "스릴러",
+    rate: "8.25",
+    openDate: "2021.04.01",
+    act: "자바독, 개리씨, 빙키",
+  },
+  {
+    title: "파이썬 부트캠프",
+    type: "드라마",
+    rate: "8.77",
+    openDate: "2021.04.12",
+    act: "캣, 파이, 씬, 뮤라",
+  },
+  {
+    title: "NGINX의 춤",
+    type: "드라마",
+    rate: "9.88",
+    openDate: "2021.04.15",
+    act: "NGINX, 노션",
+  },
+  {
+    title: "어쩌다 밋업",
+    type: "드라마",
+    rate: "9.25",
+    openDate: "2021.04.17",
+    act: "라이캣, 자바독, 소울곰",
+  },
+  {
+    title: "파이게임 어드벤처",
+    type: "판타지게임",
+    rate: "8.56",
+    openDate: "2021.04.05",
+    act: "개리씨, 파이썬",
+  },
+  {
+    title: "파이큐티의 사랑",
+    type: "로맨스",
+    rate: "8.81",
+    openDate: "2021.04.08",
+    act: "파이썬, 큐티",
+  },
+  {
+    title: "MBIT는 내 운명",
+    type: "로맨스",
+    rate: "9.02",
+    openDate: "2021.04.12",
+    act: "라이캣, 개리씨, 자바독",
+  },
+];
+
+let movieImg = [
+  { img1: "../images/포스터_위니브월드.png" },
+  { img2: "../images/포스터_눈떠보니자바.png" },
+  { img2: "../images/포스터_부트캠프.jpg" },
+  { img2: "../images/포스터_앤진액스.png" },
+  { img2: "../images/포스터_어쩌다밋업.png" },
+  { img2: "../images/포스터_파이게임.jpg" },
+  { img2: "../images/포스터_파이큐티.png" },
+  { img2: "../images/포스터_MBIT.png" },
+];
+
+const movieTitle = document.getElementsByClassName("title-moive");
+movieTitle;
