7c3551c6c6b35c71572a23f1ceccc58eb548c22b
0131ryu <131ryuji@gmail.com>
2022-06-09 16:00:58 +0900

    useState 사용한 hook 방식

--- rating/src/StarRating.js
+++ rating/src/StarRating.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-export default function StarRating() {
-  return [
-    <FaStar color="red" />,
-    <FaStar color="red" />,
-    <FaStar color="red" />,
-    <FaStar color="grey" />,
-    <FaStar color="grey" />,
-  ];
+const Star = ({ selected = false, onSelect = (f) => f }) => (
+  <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
+);
+
+const createArray = (length) => [...Array(length)];
+
+export default function StarRating({ totalStars = 5 }) {
+  const [SelectedStars, setSelectedStars] = useState(0);
+
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={SelectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {SelectedStars} of {totalStars} stars
+      </p>
+    </>
+  );
 }
