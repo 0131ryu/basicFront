f67d518c5c702c9ea691f969b916d092fda673e8
0131ryu <131ryuji@gmail.com>
2022-06-13 15:07:21 +0900

    배열 항목 추가 제거, 수정

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,20 +1,79 @@
-import Hello from "./Hello";
-import Wrapper from "./Wrapper";
-import "./App.css";
-import Counter from "./Counter";
-import InputSample from "./InputSample";
+import React, { useRef, useState } from "react";
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+      active: true,
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+      active: false,
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+      active: false,
+    },
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    // setUsers([...users, user]);
+    setUsers(users.concat(user));
+    setInputs({
+      username: "",
+      email: "",
+    });
+    nextId.current += 1;
+  };
+
+  const onRemove = (id) => {
+    //user.id가 id인 것을 제거함
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  const onToggle = (id) => {
+    setUsers(
+      users.map((user) =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
+
   return (
     <div>
-      <Wrapper>
-        <Hello name="react" color="red" isSpecial={true} />
-        <Hello color="pink" />
-      </Wrapper>
-      <Counter />
-      <InputSample />
-      <UserList />
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </div>
   );
 }
